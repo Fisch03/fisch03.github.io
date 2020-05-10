6873edde0c2d9dbb151de106abab60d16547c5d1
Samyocord <samyocord@gmail.com>
2020-05-10 14:00:10 +0200

    darkmode finally works more than once

--- darkmode.js
+++ darkmode.js
@@ -4,23 +4,33 @@ function changeToDarkmode(imageButton){
     if (!darkmodeEnabled){
         console.log("changed to darkmode");
         darkmodeEnabled = true;
+        document.body.classList = "darkmode";
         document.getElementById("GitHubLogo").src = "resources/GitHub-logo_dark.svg";
         document.getElementById("InstagramLogo").src = "resources/Instagram-logo_dark.svg";
         document.getElementById("SoundcloudLogo").src = "resources/Soundcloud-logo_dark.svg";
         document.getElementById("YoutubeLogo").src = "resources/Youtube-logo_dark.svg";
         document.getElementById("ArrowBackButton").src = "resources/arrowback_dark.svg";
-        document.body.classList += "darkmode";
+        document.getElementById("LogoCode").src = "resources/logo/logo1_dark.svg";
+        document.getElementById("Logo3D").src = "resources/logo/logo2_dark.svg";
+        document.getElementById("LogoMusic").src = "resources/logo/logo3_dark.svg";
+        document.getElementById("LogoOuter").src = "resources/logo/logoouter_dark.svg";
+
         
         imageButton.src = "resources/lightmode.svg";
     } else {
         console.log("changed to lightmode (ew)");
         darkmodeEnabled = false;
+
+        document.body.classList = null;
         document.getElementById("GitHubLogo").src = "resources/GitHub-logo.svg";
         document.getElementById("InstagramLogo").src = "resources/Instagram-logo.svg";
         document.getElementById("SoundcloudLogo").src = "resources/Soundcloud-logo.svg";
         document.getElementById("YoutubeLogo").src = "resources/Youtube-logo.svg";
         document.getElementById("ArrowBackButton").src = "resources/arrowback.svg";
-        document.body.classList -= "darkmode";
+        document.getElementById("LogoCode").src = "resources/logo/logo1.svg";
+        document.getElementById("Logo3D").src = "resources/logo/logo2.svg";
+        document.getElementById("LogoMusic").src = "resources/logo/logo3.svg";
+        document.getElementById("LogoOuter").src = "resources/logo/logoouter.svg";
         imageButton.src = "resources/darkmode.svg";
     }
 }
